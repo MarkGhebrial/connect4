09fdfea6c558541dee2e124c3a6c85aa59ebf36a
MarkGhebrial <mghebrialjr@icloud.com>
2026-09-03 22:07:11 -0700

    Add rudimentary `Board::from_c4e` test

--- c4board/src/test/board_test.rs
+++ c4board/src/test/board_test.rs
@@ -7,3 +7,11 @@ fn is_valid_test() {
 fn iter_moves_test() {
     todo!("Test for Board::iter_moves() is not implemented yet");
 }
+
+#[test]
+fn from_c4e_test() {
+    use crate::board::Board;
+
+    let board = Board::from_c4e("y;;yyryry;yyrryr;ryyrrr;;").unwrap();
+    assert!(board.is_valid());
+}
